81ba9f33ff8a0f8f58e8ef7ea3d9ca36a6a5edbf
ablhmne2 <amel.boulhamane@efrei.net>
2025-10-20 14:31:47 +0200

    Function cell.h and cell.c

--- cell.c
+++ cell.c
@@ -2,4 +2,16 @@
 // Created by Boulhamane Amel on 20/10/2025.
 //
 
+#include "cell.h"
 
+#include <stdlib.h>
+
+t_cell *create_cell(int arrival, float probability) {
+    t_cell *cell = (t_cell *)malloc(sizeof(t_cell));
+    if (cell ==NULL) {
+        return NULL;
+    }
+    cell->arrival = arrival;
+    cell->probability = probability;
+    return cell;
+}
