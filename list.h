388b0a48a6c8bdccfc51f6f5261b4002b24bd7e1
anais <anais.delfour@efrei.net>
2025-10-20 14:22:45 +0200

    added: list.h

---
+++ list.h
@@ -0,0 +1,12 @@
+//
+// Created by anais on 20/10/2025.
+//
+
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct s_std_list
+{
+    p_cell head;
+} t_std_list;
+#endif //LIST_H
--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 
 int main() {
     printf("Hello, World!\n");
-    printf("it work");
+    printf("it works");
     return 0;
 }
