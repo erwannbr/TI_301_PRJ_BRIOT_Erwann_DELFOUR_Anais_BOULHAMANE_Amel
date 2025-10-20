c7e939f6ebbbbf9d74ca44909500561301872bc4
ablhmne2 <amel.boulhamane@efrei.net>
2025-10-20 15:16:35 +0200

    Function adjacency_list.h and adjacency_list.c + directories

--- adjacency_list/adjacency_list.c
+++ adjacency_list/adjacency_list.c
@@ -2,3 +2,18 @@
 // Created by Boulhamane Amel on 20/10/2025.
 //
 
+t_adjacency_list* empty_adjacency_list(int size) {
+    t_adjacency_list *p_adj_list = malloc(sizeof(t_adjacency_list));
+    if (p_adj_list == NULL) {
+        return NULL;
+    }
+    p_adj_list->size = size;
+    p_adj_list->list = malloc(size*sizeof(t_std_list));
+    if (p_adj_list->list == NULL) {
+        return NULL;
+    }
+    for (int i = 0; i < size; i++) {
+        p_adj_list->list[i].head = NULL;
+    }
+    return p_adj_list;
+}
--- adjacency_list/adjacency_list.h
+++ adjacency_list/adjacency_list.h
@@ -11,18 +11,6 @@ typedef struct s_adjacency_list {
     int size;
 }t_adjacency_list;
 
-t_adjacency_list* empty_adjacency_list(int size) {
-    t_adjacency_list *adjacency_list;
-    adjacency_list.size = size;
-
-    adjacency_list.list = create_std_list(size);
-    if (adjacency_list.list == NULL) {
-        return NULL;
-    }
-    for (int i = 0; i < size; i++) {}
-
-
-
-}
+t_adjacency_list* empty_adjacency_list(int size);
 
 #endif //ADJACENCY_LIST_H
