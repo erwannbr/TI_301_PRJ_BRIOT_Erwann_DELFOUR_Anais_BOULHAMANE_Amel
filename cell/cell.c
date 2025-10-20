f835ccc51087a5938ead83fd5ca335b4105181fe
erwannbr <erwann.briot@efrei.net>
2025-10-20 14:59:08 +0200

    de

--- cell/cell.c
+++ cell/cell.c
@@ -7,10 +7,13 @@
 
 t_cell *create_cell(int arrival, float probability) {
     t_cell *cell = (t_cell *)malloc(sizeof(t_cell));
+
     if (cell ==NULL) {
         return NULL;
     }
+
     cell->arrival = arrival;
     cell->probability = probability;
+    
     return cell;
 }
--- list.c
+++ list.c
@@ -5,18 +5,24 @@
 
 t_std_list * CreateEmptyList () {
     t_std_list *list = (t_std_list*)malloc(sizeof(t_std_list));
+
     list->head = NULL;
+
     return list;
 }
 
 void DisplayList (t_std_list list) {
     t_cell* curr = list.head;
     printf("[head @]");
+
     while (curr->next != NULL) {
         printf ("(%d,%f) @->", curr->arrival,curr->probability);
         curr=curr->next;
     }
+    
     printf ("(%d,%f)" , curr->arrival,curr->probability);
+
+    return;
 }
 
 void AddCell(t_std_list *list, p_cell cell) {
