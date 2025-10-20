cca38546cb769568c285dfae2f3e5fb211976210
anais <anais.delfour@efrei.net>
2025-10-20 15:57:41 +0200

    added: cmake

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include "adjacency_list/adjacency_list.h"
+
 
 
 int main() {
     printf("Hello, World!\n");
     printf("it works");
+    t_adjacency_list new_adj_list = readGraph("DATA/exemple1.txt");
+    display_adjacency_list(new_adj_list);
     return 0;
+
 }
