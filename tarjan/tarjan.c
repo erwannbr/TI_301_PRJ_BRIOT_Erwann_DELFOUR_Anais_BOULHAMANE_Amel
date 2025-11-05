c4eb9ffd62ee010a9a54754a1e7688fadf905824
anais <anais.delfour@efrei.net>
2025-11-05 18:50:33 +0100

    Verification que les pointeurs sont non-nuls

--- tarjan/tarjan.c
+++ tarjan/tarjan.c
@@ -11,8 +11,8 @@
 t_tarjan_vertex * CreateArr (int n) {
     t_tarjan_vertex * array = malloc (n * sizeof (t_tarjan_vertex));
     if (!array) {
-        printf("Allocation failed");
-        return NULL;
+        printf("Allocation failed\n");
+        exit (EXIT_FAILURE);
     }
     for (int i=0; i<n; i++) {
         array[i].id = i+1;
@@ -23,14 +23,19 @@ t_tarjan_vertex * CreateArr (int n) {
     return array;
 }
 
-p_class CreateClass (char * name_of_class) {
+p_class CreateClass (const char * name_of_class) {
     p_class c = malloc(sizeof(t_class));
     if (!c) {
-        printf("Allocation of class failed");
-        return NULL;
+        printf("Allocation of class failed\n");
+        exit (EXIT_FAILURE);
     }
     strcpy(c->name, name_of_class);
     c->vertices = malloc (4 * sizeof (int));
+    if (!c ->vertices) {
+        free(c);
+        printf("Allocation of vertices failed\n");
+        exit(EXIT_FAILURE);
+    }
     c->nb_vertices = 0;
     c->size = 4;
     return c;
@@ -38,36 +43,51 @@ p_class CreateClass (char * name_of_class) {
 }
 
 void AddVertexToClass (p_class c, int vertex_id) {
+    if (!c) return;
     if (c->nb_vertices >= c->size) {
         c->size *=2;
-        c->vertices = realloc (c->vertices, c->size * sizeof(int));
+        int *temp = realloc(c->vertices, c->size * sizeof(int));
+        if (!temp) {
+            printf("Reallocation failed\n");
+            exit(EXIT_FAILURE);
+        }
+        c->vertices = temp;
     }
-    c->vertices [c->nb_vertices++] = vertex_id;
-    return;
+    c->vertices[c->nb_vertices++] = vertex_id;
 }
 
 t_partition *CreatePartition() {
     t_partition *p = malloc(sizeof(t_partition));
     if (!p) {
-        printf("Allocation of partition failed");
-        return NULL;
+        printf("Allocation of partition failed\n");
+        exit (EXIT_FAILURE);
     }
     p->classes = malloc (4 * sizeof(t_class*));
+    if (!p->classes) {
+        free(p);
+        printf("Allocation of classes array failed\n");
+    }
     p->nb_class = 0;
     p->size = 4;
     return p;
 }
 
 void AddClassToPartition (p_partition p, p_class c) {
+    if (!p || !c) return;
     if (p->nb_class >= p->size) {
         p->size *= 2;
-        p->classes = realloc (p->classes, p->size * sizeof(t_class*));
+        t_class **temp = realloc(p->classes, p->size * sizeof(t_class*));
+        if (!temp) {
+            printf("Reallocation failed\n");
+            exit(EXIT_FAILURE);
+        }
+        p->classes = temp;
     }
-    p->classes [p->nb_class++] = c;
-    return;
+    p->classes[p->nb_class++] = c;
 }
 
 void Parcours (int v, t_adjacency_list graph, p_tarjan_vertex Ver, t_stack *S, p_partition part, int *index) {
+    if (!Ver || !S || !part || !index) return;
     Ver[v].class_nb = *index;
     Ver[v].link_nb = *index;
     (*index)++;
@@ -90,7 +110,7 @@ void Parcours (int v, t_adjacency_list graph, p_tarjan_vertex Ver, t_stack *S, p
     }
     if (Ver[v].link_nb == Ver[v].class_nb) {
         char name[10];
-        sprintf(name, "C%d", part->nb_class + 1);
+        snprintf(name, sizeof(name), "C%d", part->nb_class + 1);
         p_class cls = CreateClass(name);
 
         int w;
@@ -108,6 +128,11 @@ p_partition Tarjan (t_adjacency_list graph) {
     int n = graph.size;
     p_tarjan_vertex Ver = CreateArr(n);
     t_stack *S = CreateStack();
+    if (!S) {
+        free(Ver);
+        printf("Stack creation failed\n");
+        exit(EXIT_FAILURE);
+    }
     p_partition part = CreatePartition();
     int index = 0;
 
@@ -117,6 +142,10 @@ p_partition Tarjan (t_adjacency_list graph) {
         }
     }
     free(Ver);
+
+    while (!isEmpty(S)) {
+        pop(S);
+    }
     free(S);
     return part;
 }
