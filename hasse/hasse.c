064e136e6177c8df9605bb974a97f84ea3d4a724
ablhmne2 <amel.boulhamane@efrei.net>
2025-11-05 17:15:20 +0100

    Part 2 - Step 2

--- hasse/hasse.c
+++ hasse/hasse.c
@@ -1,6 +1,58 @@
 #include <malloc.h>
 #include "hasse.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+void links_init(t_link_array *a) {
+    a->links = NULL;
+    a->size = 0;
+    a->length = 0;
+}
+
+void links_add(t_link_array *a, int start, int end) {
+    for (int i = 0; i < a->size; ++i) {
+        if (a->links[i].start == start && a->links[i].end == end) return;
+    }
+    if (a->size == a->length) {
+        a->length = (a->length == 0) ? 8 : a->length * 2;
+        a->links = (t_link*)realloc(a->links, a->length * sizeof *a->links);
+    }
+    a->links[a->size++] = (t_link){ start, end };
+}
+
+int *create_array_vertex_to_class(int vertex_count, const t_partition *P) {
+    int *map = (int*)malloc(vertex_count * sizeof *map);
+    if (!map) return NULL;
+    for (int v = 0; v < vertex_count; ++v) map[v] = -1;
+
+    for (int c = 0; c < P->nb_class; ++c) {
+        const t_class *cls = P->classes[c];
+        for (int k = 0; k < cls->nb_vertices; ++k) {
+            int v_id_1based = cls->vertices[k]; // ex: 1..N
+            int v_index = v_id_1based - 1;      // -> 0..N-1
+            if (0 <= v_index && v_index < vertex_count)
+                map[v_index] = c;
+        }
+    }
+    return map;
+}
+
+void list_class_links(int vertex_count, Vertex *graph, int *vertex_to_class, t_link_array *class_links)
+{
+    for (int vertex = 0; vertex < vertex_count; vertex++) {
+        int class_from = vertex_to_class[vertex];
+
+        for (int n = 0; n < graph[vertex].neighbor_count; n++) {
+            int neighbor = graph[vertex].neighbors[n];
+            int class_to = vertex_to_class[neighbor];
+
+            if (class_from != class_to) {
+                links_add(class_links, class_from, class_to);
+            }
+        }
+    }
+}
 
 void removeTransitiveLinks(t_link_array *p_link_array)
 {
@@ -15,7 +67,7 @@ void removeTransitiveLinks(t_link_array *p_link_array)
             if (j != i)
             {
                 t_link link2 = p_link_array->links[j];
-                if (link1.from == link2.from)
+                if (link1.start == link2.start)
                 {
                     // look for a link from link2.to to link1.to
                     int k = 0;
@@ -24,7 +76,7 @@ void removeTransitiveLinks(t_link_array *p_link_array)
                         if (k != j && k != i)
                         {
                             t_link link3 = p_link_array->links[k];
-                            if ((link3.from == link2.to) && (link3.to == link1.to))
+                            if ((link3.start == link2.end) && (link3.start == link1.end))
                             {
                                 to_remove = 1;
                             }
--- hasse/hasse.h
+++ hasse/hasse.h
@@ -1,6 +1,29 @@
 #ifndef __HASSE_H__
 #define __HASSE_H__
+#include "../tarjan/tarjan.h"
 
+typedef struct {
+    int start;
+    int end;
+} t_link;
+
+typedef struct {
+    t_link *links;
+    int size;
+    int length;
+} t_link_array;
+
+typedef struct {
+    int *neighbors;
+    int neighbor_count;
+} Vertex;
+
+void links_init(t_link_array *a);
+void links_add(t_link_array *a, int start, int end);
+
+int *create_array_vertex_to_class(int vertex_count, const t_partition *P);
+
+void list_class_links(int vertex_count, Vertex *graph, int *vertex_to_class, t_link_array *class_links);
 void removeTransitiveLinks(t_link_array *p_link_array);
 
 /**
