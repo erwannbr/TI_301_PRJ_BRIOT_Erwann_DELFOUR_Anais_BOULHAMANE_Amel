5226cf67927c574a2b803f02f96d80dce8efc900
ablhmne2 <amel.boulhamane@efrei.net>
2025-11-10 14:37:11 +0100

    Part 3 - Step 2

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -1,19 +1,24 @@
-//
-// Created by anais on 10/11/2025.
-//
-
 #ifndef MATRIX_H
 #define MATRIX_H
 #include "../adjacency_list/adjacency_list.h"
+#include "../tarjan/tarjan.h"
+
 typedef struct s_matrix {
     int size;
     float **data;
 } t_matrix, *p_matrix;
 
 p_matrix CreateEmptyMatrix(int n);
+
 p_matrix CreateMatFromAdjList(t_adjacency_list graph);
+
 void CopyMatrix(p_matrix mat, p_matrix matsrc);
+
 p_matrix MultiplyMatrices(p_matrix A, p_matrix B);
+
 float DiffMatrix(p_matrix M, p_matrix N);
+
 void printMatrix(p_matrix M);
+
+p_matrix SubMatrixByComponent(p_matrix M, t_partition part, int compo_index);
 #endif //MATRIX_H
